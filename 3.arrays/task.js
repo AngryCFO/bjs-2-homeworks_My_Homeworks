78d2a79e10406b4c5cf8dc407f26722225396481
AngryCFO <147319756+AngryCFO@users.noreply.github.com>
2024-08-23 22:10:06 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -8,10 +8,14 @@ function compareArrays(arr1, arr2) {
 // Задача 2. Фильтрация и преобразование массива
 function getUsersNamesInAgeRange(users, gender) {
   const filteredUsers = users.filter(user => user.gender === gender);
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
   const ages = filteredUsers.map(user => user.age);
   const averageAge = ages.reduce((sum, age) => sum + age, 0) / ages.length;
-  return filteredUsers.length > 0 ? averageAge : 0;
+  return averageAge;
 }
+
 
 // Пример использования
 const people = [

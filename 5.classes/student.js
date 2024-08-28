2052ca52706e63c0da3a26740f61fd5ae4e45021
AngryCFO <147319756+AngryCFO@users.noreply.github.com>
2024-08-28 23:31:31 +0300

    Update student.js

--- 5.classes/student.js
+++ 5.classes/student.js
@@ -1,1 +1,30 @@
+// Задача 3. Журнал успеваемости
+class Student {
+  constructor(name) {
+    this.name = name;
+    this.marks = {};
+  }
 
+  addMark(mark, subject) {
+    if (mark >= 2 && mark <= 5) {
+      if (!this.marks[subject]) {
+        this.marks[subject] = [];
+      }
+      this.marks[subject].push(mark);
+    }
+  }
+
+  getAverageBySubject(subject) {
+    if (!this.marks[subject]) {
+      return 0;
+    }
+    const sum = this.marks[subject].reduce((acc, mark) => acc + mark, 0);
+    return sum / this.marks[subject].length;
+  }
+
+  getAverage() {
+    const subjects = Object.keys(this.marks);
+    const sum = subjects.reduce((acc, subject) => acc + this.getAverageBySubject(subject), 0);
+    return sum / subjects.length;
+  }
+}

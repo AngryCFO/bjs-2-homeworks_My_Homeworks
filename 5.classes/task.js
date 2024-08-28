261367e11abc8a60a5f3fa2bab0395fd42257bac
AngryCFO <147319756+AngryCFO@users.noreply.github.com>
2024-08-28 23:17:04 +0300

    Update task.js

---
+++ 5.classes/task.js
@@ -0,0 +1,122 @@
+// Задача 1. Печатное издание
+class PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.pagesCount = pagesCount;
+    this.state = 100;
+    this.type = null;
+  }
+
+  get state() {
+    return this._state;
+  }
+
+  set state(newState) {
+    if (newState < 0) {
+      this._state = 0;
+    } else if (newState > 100) {
+      this._state = 100;
+    } else {
+      this._state = newState;
+    }
+  }
+
+  fix() {
+    this.state = this.state * 1.5;
+  }
+}
+
+class Magazine extends PrintEditionItem {
+  constructor(name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.type = "magazine";
+  }
+}
+
+class Book extends PrintEditionItem {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
+    this.author = author;
+    this.type = "book";
+  }
+}
+
+class NovelBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "novel";
+  }
+}
+
+class FantasticBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "fantastic";
+  }
+}
+
+class DetectiveBook extends Book {
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
+    this.type = "detective";
+  }
+}
+
+// Задача 2. Библиотека
+class Library {
+  constructor(name) {
+    this.name = name;
+    this.books = [];
+  }
+
+  addBook(book) {
+    if (book.state > 30) {
+      this.books.push(book);
+    }
+  }
+
+  findBookBy(type, value) {
+    return this.books.find((book) => book[type] === value) || null;
+  }
+
+  giveBookByName(bookName) {
+    const book = this.findBookBy("name", bookName);
+    if (book) {
+      this.books = this.books.filter((b) => b !== book);
+      return book;
+    }
+    return null;
+  }
+}
+
+// Задача 3. Журнал успеваемости
+class Student {
+  constructor(name) {
+    this.name = name;
+    this.marks = {};
+  }
+
+  addMark(mark, subject) {
+    if (mark >= 2 && mark <= 5) {
+      if (!this.marks[subject]) {
+        this.marks[subject] = [];
+      }
+      this.marks[subject].push(mark);
+    }
+  }
+
+  getAverageBySubject(subject) {
+    if (!this.marks[subject]) {
+      return 0;
+    }
+    const sum = this.marks[subject].reduce((acc, mark) => acc + mark, 0);
+    return sum / this.marks[subject].length;
+  }
+
+  getAverage() {
+    const subjects = Object.keys(this.marks);
+    const sum = subjects.reduce((acc, subject) => acc + this.getAverageBySubject(subject), 0);
+    return sum / subjects.length;
+  }
+}

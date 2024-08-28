baa836750b13826045b15f3b49ed2c8f725d3adf
AngryCFO <147319756+AngryCFO@users.noreply.github.com>
2024-08-29 00:15:56 +0300

    Update task.js

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -98,6 +98,9 @@ class Student {
   }
 
   addMark(mark, subject) {
+    if (typeof mark !== 'number' || typeof subject !== 'string') {
+      throw new Error('Неправильный тип данных');
+    }
     if (mark >= 2 && mark <= 5) {
       if (!this.marks[subject]) {
         this.marks[subject] = [];
@@ -107,20 +110,21 @@ class Student {
   }
 
   getAverageBySubject(subject) {
-    if (!this.marks[subject]) {
+    if (!this.marks[subject] || this.marks[subject].length === 0) {
       return 0;
     }
     const sum = this.marks[subject].reduce((acc, mark) => acc + mark, 0);
     return sum / this.marks[subject].length;
   }
 
-   getAverage() {
-  const subjects = Object.keys(this.marks);
-  if (subjects.length === 0) {
-    return 0;
+  getAverage() {
+    const subjects = Object.keys(this.marks);
+    if (subjects.length === 0) {
+      return 0;
+    }
+    const sum = subjects.reduce((acc, subject) => acc + this.marks[subject].reduce((acc, mark) => acc + mark, 0), 0);
+    return sum / subjects.reduce((acc, subject) => acc + this.marks[subject].length, 0);
   }
-  const sum = subjects.reduce((acc, subject) => acc + this.getAverageBySubject(subject), 0);
-  return sum / subjects.length;
 }
 
 }

e983f1f7565cd79466886d6f95f3bd4bfc52dd1a
AngryCFO <147319756+AngryCFO@users.noreply.github.com>
2024-09-17 02:24:25 +0300

    Update task.js

--- 7.async/task.js
+++ 7.async/task.js
@@ -4,58 +4,40 @@ class AlarmClock {
     this.intervalId = null;
   }
 
-  addClock(time, callback) {
+  addClock(time, callback, id) {
     if (!time || !callback) {
       throw new Error('Отсутствуют обязательные аргументы');
     }
-
-    const timeRegex = /^([01][0-9]|2[0-3]):[0-5][0-9]$/;
-    if (!timeRegex.test(time)) {
-      throw new Error('Неправильный формат времени');
+    if (typeof callback !== 'function') {
+      throw new Error('Колбэк должен быть функцией');
     }
-
-    const existingAlarm = this.alarmCollection.find(alarm => alarm.time === time);
-    if (existingAlarm) {
-      console.warn('Уже присутствует звонок на это же время');
-      return;
-    }
-
-    this.alarmCollection.push({ time, callback, canCall: true });
+    const newAlarm = { time, callback, canCall: true, id: id || Date.now().toString() };
+    this.alarmCollection.push(newAlarm);
+    return newAlarm.id;
   }
 
   removeClock(time) {
-    const index = this.alarmCollection.findIndex(alarm => alarm.time === time);
-    if (index !== -1) {
-      this.alarmCollection.splice(index, 1);
-    }
+    this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
   }
 
   getCurrentFormattedTime() {
-    const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    return `${hours}:${minutes}`;
+    return new Date().toLocaleTimeString("ru-RU", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
   }
 
   start() {
-    if (this.intervalId) {
-      return;
+    if (!this.intervalId) {
+      this.intervalId = setInterval(() => {
+        this.alarmCollection.forEach(alarm => {
+          if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall) {
+            alarm.canCall = false;
+            alarm.callback();
+          }
+        });
+      }, 1000);
     }
-
-    if (this.alarmCollection.length === 0) {
-      console.warn('Коллекция звонков пуста');
-      return;
-    }
-
-    this.intervalId = setInterval(() => {
-      const currentTime = this.getCurrentFormattedTime();
-      this.alarmCollection.forEach(alarm => {
-        if (alarm.time === currentTime && alarm.canCall) {
-          alarm.canCall = false;
-          alarm.callback();
-        }
-      });
-    }, 1000);
   }
 
   stop() {
@@ -65,31 +47,14 @@ class AlarmClock {
     }
   }
 
+  clearAlarms() {
+    this.stop();
+    this.alarmCollection = [];
+  }
+
   resetAllCalls() {
     this.alarmCollection.forEach(alarm => {
       alarm.canCall = true;
     });
   }
-
-  clearAlarms() {
-    this.stop();
-    this.alarmCollection.length = 0;
-  }
 }
-
-// Пример использования:
-const alarmClock = new AlarmClock();
-
-alarmClock.addClock('08:00', () => console.log('Пора вставать!'));
-alarmClock.addClock('12:00', () => console.log('Время обеда!'));
-
-alarmClock.start();
-
-// Чтобы остановить будильник
-// alarmClock.stop();
-
-// Чтобы сбросить возможность запуска всех звонков
-// alarmClock.resetAllCalls();
-
-// Чтобы удалить все звонки
-// alarmClock.clearAlarms();

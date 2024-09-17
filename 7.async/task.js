a84c59598bd0e56bd8f8a88ee7598ba7bf023c33
AngryCFO <147319756+AngryCFO@users.noreply.github.com>
2024-09-17 03:54:05 +0300

    Update task.js

--- 7.async/task.js
+++ 7.async/task.js
@@ -9,25 +9,21 @@ class AlarmClock {
       throw new Error('Отсутствуют обязательные аргументы');
     }
 
-    const timeRegex = /^([01][0-9]|2[0-3]):[0-5][0-9]$/;
-    if (!timeRegex.test(time)) {
-      throw new Error('Неправильный формат времени');
-    }
-
     const existingAlarm = this.alarmCollection.find(alarm => alarm.time === time);
     if (existingAlarm) {
       console.warn('Уже присутствует звонок на это же время');
       return;
     }
 
-    this.alarmCollection.push({ time, callback, canCall: true });
+    this.alarmCollection.push({
+      callback,
+      time,
+      canCall: true
+    });
   }
 
   removeClock(time) {
-    const index = this.alarmCollection.findIndex(alarm => alarm.time === time);
-    if (index !== -1) {
-      this.alarmCollection.splice(index, 1);
-    }
+    this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
   }
 
   getCurrentFormattedTime() {
@@ -38,12 +34,7 @@ class AlarmClock {
   }
 
   start() {
-    if (this.intervalId) {
-      return;
-    }
-
-    if (this.alarmCollection.length === 0) {
-      console.warn('Коллекция звонков пуста');
+    if (this.intervalId !== null) {
       return;
     }
 
@@ -59,10 +50,8 @@ class AlarmClock {
   }
 
   stop() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-      this.intervalId = null;
-    }
+    clearInterval(this.intervalId);
+    this.intervalId = null;
   }
 
   resetAllCalls() {
@@ -72,28 +61,7 @@ class AlarmClock {
   }
 
   clearAlarms() {
-    this.stop(); // Останавливаем интервал
-    this.alarmCollection.length = 0; // Полностью очищаем коллекцию звонков
-  }
-
-  getAlarmCollection() {
-    return this.alarmCollection; // Возвращаем коллекцию звонков
+    this.stop();
+    this.alarmCollection = [];
   }
 }
-
-// Пример использования:
-const alarmClock = new AlarmClock();
-
-alarmClock.addClock('08:00', () => console.log('Пора вставать!'));
-alarmClock.addClock('12:00', () => console.log('Время обеда!'));
-
-alarmClock.start();
-
-// Чтобы остановить будильник
-// alarmClock.stop();
-
-// Чтобы сбросить возможность запуска всех звонков
-// alarmClock.resetAllCalls();
-
-// Чтобы удалить все звонки
-// alarmClock.clearAlarms();

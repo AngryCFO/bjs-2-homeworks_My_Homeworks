9e73512b1711281233f6179983f041a68d32d7cf
AngryCFO <147319756+AngryCFO@users.noreply.github.com>
2024-09-17 04:35:05 +0300

    Update task.js

--- 7.async/task.js
+++ 7.async/task.js
@@ -9,6 +9,12 @@ class AlarmClock {
       throw new Error('Отсутствуют обязательные аргументы');
     }
 
+    const existingAlarm = this.alarmCollection.find(alarm => alarm.time === time);
+    if (existingAlarm) {
+      console.warn('Уже присутствует звонок на это же время');
+      return;
+    }
+
     this.alarmCollection.push({
       callback,
       time,
